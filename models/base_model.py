94c80144294d3190cff5b05d08c4b7ec78a1c08c
grem62 <gremontmatheo22@gmail.com>
2023-07-05 12:52:11 +0200

    changement doc

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ import """
-
-
 from datetime import datetime
 import uuid
-import models
 
 
 class BaseModel:
-
+    """
+    Construct a new BaseModel
+    """
     def __init__(self):
-        """Construct a new BaseModel """
+        
 
         self.id = str(uuid.uuid4())
 
@@ -18,22 +17,12 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Returns a string representation of the object."""
-
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Updates attribute of the object with the current datetime.
-        """
-
         self.update_at = datetime.now()
 
     def to_dict(self):
-        """
-        Convert the object to a dictionary representation.
-        """
-
         dictionnaire = self.__dict__.copy()
         dictionnaire['__class__'] = self.__class__.__name__
         dictionnaire['created_at'] = self.created_at.isoformat()
