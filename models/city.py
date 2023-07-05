0e496948bf1e3c273b590ceffd8fece856bda456
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-05 15:52:52 +0200

    string empty

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" city module """
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class from Basemodel """
+    state_id = ""
+    name = ""
