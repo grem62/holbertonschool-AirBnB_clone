22d35c34fe946de148c5c979143f1f2ed024482e
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-05 10:57:10 +0200

    Correction to dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" import """
 
 from datetime import datetime
 import uuid
@@ -8,21 +8,28 @@ import uuid
 class BaseModel:
 
     def __init__(self):
+        """Construct a new BaseModel """
         self.id = str(uuid.uuid4)
 
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """ Returns a string representation of the object."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+        Updates the 'update_at' attribute of the object with the current datetime.
+        """
         self.update_at = datetime.now()
 
     def to_dict(self):
-
+        """
+        Convert the object to a dictionary representation.
+        """
         dictionnaire = self.__dict__.copy()
         dictionnaire['__class__'] = self.__class__.__name__
-        dictionnaire['self.created_at'] = self.created_at.isoformat()
-        dictionnaire['self.updated_at'] = self.updated_at.isoformat()
+        dictionnaire['created_at'] = self.created_at.isoformat()
+        dictionnaire['updated_at'] = self.updated_at.isoformat()
         return dictionnaire
