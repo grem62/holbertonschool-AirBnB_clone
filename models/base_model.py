39f33eb6919b6dfc804eae4bf78bea4e854eb387
grem62 <gremontmatheo22@gmail.com>
2023-07-05 14:12:43 +0200

    add the task 4 kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,19 +8,29 @@ class BaseModel:
     """"Class that defines all common
     attributes/methods for other classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        """def init str"""
 
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
+        """string name"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """save the date and time"""
         update_at = datetime.now()
 
     def to_dict(self):
+        """dictionnary"""
         dictionnaire = self.__dict__.copy()
         dictionnaire['__class__'] = self.__class__.__name__
         dictionnaire['created_at'] = self.created_at.isoformat()
