fe73ec573bc96dba1980c046e00b4cb1b0e685d1
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-06 16:50:14 +0200

    Update class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 """new class FileStorage"""
 
+
+import cmd
 import json
+import shlex
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
 
     __file_path = "file.json"
-    __objects = {}
+    __objects = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "Place": Place,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
+    }
 
     def all(self):
         return self.__objects
-    
+
     def new(self, obj):
         """ sets in __objects the obj with key
             <obj class name>.id
