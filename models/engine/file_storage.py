c0d455d915ce87ae4162c5ca6c77826c6c85b15a
grem62 <gremontmatheo22@gmail.com>
2023-07-07 10:38:46 +0200

    changement fil

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,31 +2,28 @@
 """new class FileStorage"""
 
 
-import cmd
 import json
-import shlex
+import os.path
 from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 class FileStorage:
-
+    """ String representing a simple data structure in JSON format.
+        ex: '{ "12": { "numbers": [1, 2, 3], "name": "John" } }'
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """ returns the dictionary __objects """
         return self.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key
-            <obj class name>.id
         """
-        key = obj.__class__.__name__ + "." + str(obj.id)
-        self.__objects[key] = obj
+        sets in __objects the obj with key <obj class name>.id
+        """
+        dict_key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects.update({dict_key: obj})
 
     def save(self):
         """ serializes __objects to the JSON file """
@@ -37,13 +34,9 @@ class FileStorage:
             json.dump(dict, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects
-        """
-        try:
-            with open(FileStorage.__file_path, 'r') as file:
-                new_dict = json.load(file)
-                cls = "__class__"
-                for key, value in new_dict.items():
-                    FileStorage.__objects[key] = eval(value[cls] + '(**value)')
-        except FileNotFoundError:
-            pass
+        """ deserializes the JSON file to __objects """
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                json_obj = json.load(f)
+            for key, val in json_obj.items():
+                self.__objects[key] = eval(val["__class__"])(**val)
