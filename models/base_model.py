c86fce221a30cc8f758438e4f1829fc5d2f7e5c6
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-09 15:11:49 +0200

    Comment

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ import """
 from datetime import datetime
-import uuid
+from uuid import uuid4
 import models
 
 
@@ -12,31 +12,31 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """def init str"""
 
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         models.storage.new(self)
 
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key in ["created_at", "updated_at"]:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key != "__class__":
                     setattr(self, key, value)
 
     def __str__(self):
-        """string name"""
+        """string name."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """save the date and time"""
-        update_at = datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """dictionnary"""
-        dictionnaire = self.__dict__.copy()
-        dictionnaire['__class__'] = self.__class__.__name__
-        dictionnaire['created_at'] = self.created_at.isoformat()
-        dictionnaire['updated_at'] = self.updated_at.isoformat()
-        return dictionnaire
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
