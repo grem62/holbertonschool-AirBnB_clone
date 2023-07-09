9b451bb1a0ba55514f7e334d70d6ec4664591ec6
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-07-09 14:07:08 +0200

    tests unittaire Correction

--- tests/tests_models/test_base_model.py
+++ tests/tests_models/test_base_model.py
@@ -1,38 +1,52 @@
-#!/usr/bin/python3
+#!/usr/bin/pyhton3
+"""Unittests module for BaseModel"""
+
 
 import unittest
-from models.base_model import BaseModel
+import os
 from datetime import datetime
-import os.path
-from models.engine.file_storage import FileStorage
 
+import models
+from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
-    def setUp(self):
-        self.base_model = BaseModel()
+    """Test class for BaseModel"""
+
+    def test_id(self):
+        """Test the id attribute"""
+        base = BaseModel()
+        self.assertEqual(str, type(base.id))
+
+    def test_created_at(self):
+        """Test created_at attribute"""
+        base = BaseModel()
+        self.assertIsInstance(base.created_at, datetime)
+
+        base.created_at = datetime.now()
+        self.assertIsNotNone(base.created_at)
+
+    def test_updated_at(self):
+        """Test updated_at attribute"""
+        base = BaseModel()
+        self.assertIsInstance(base.updated_at, datetime)
+
+        base.created_at = datetime.now()
+        self.assertIsNotNone(base.updated_at)
+
+    def test__str__(self):
+        """Test __str__ method"""
+        self.assertEqual(str, type(BaseModel.__str__(self)))
 
     def test_save(self):
-        my_model = BaseModel()
-        old_updated_at = my_model.updated_at
-        my_model.save()
-        self.assertNotEqual(old_updated_at, my_model.updated_at)
+        """Test save method"""
+        base = BaseModel()
+        base.name = "MyModel"
+        base.save()
+        self.assertNotEqual(base.created_at, base.updated_at)
+
+        with open("file.json", "r", encoding='utf-8') as f:
+            self.assertIn(base.name, f.read())
 
 
-    def test_to_dict_returns_dictionary_representation(self):
-        dictionary = self.base_model.to_dict()
-
-        self.assertIsInstance(dictionary, dict)
-        self.assertEqual(dictionary['__class__'], 'BaseModel')
-        self.assertEqual(dictionary['created_at'], self.base_model.created_at.isoformat())
-        self.assertEqual(dictionary['updated_at'], self.base_model.updated_at.isoformat())
-
-    def test_str_returns_string_representation(self):
-        string_representation = str(self.base_model)
-
-        self.assertIsInstance(string_representation, str)
-        self.assertIn('BaseModel', string_representation)
-        self.assertIn(self.base_model.id, string_representation)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
